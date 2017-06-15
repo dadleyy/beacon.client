ad586d272e52b6a628adc14faaf016c4bc81e58d
Danny Hadley <dadleyy@gmail.com>
2017-06-14 21:10:20 -0400

    initial implementation - user input from stdin controller

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
 import "fmt"
+import "github.com/hink/go-blink1"
 
 func main() {
-	fmt.Println("vim-go")
+	d, e := blink1.OpenNextDevice()
+
+	if e != nil {
+		fmt.Printf("err: %s", e.Error())
+		return
+	}
+
+	defer d.Close()
+
+	for {
+		var in string
+
+		if _, e := fmt.Scanln(&in); e != nil {
+			fmt.Printf("err: %s", e.Error())
+			return
+		}
+
+		if in == "exit" {
+			fmt.Printf("exiting...\n")
+			break
+		}
+
+		var s blink1.State
+		switch in {
+		case "red":
+			s = blink1.State{Red: 255}
+		case "blue":
+			s = blink1.State{Blue: 255}
+		case "green":
+			s = blink1.State{Green: 255}
+		}
+
+		d.SetState(s)
+
+	}
+
+	d.SetState(blink1.State{})
 }
