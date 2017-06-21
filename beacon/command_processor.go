f04556eeac7d0bcffa65df1cc09a610418f1506d
Danny Hadley <dadleyy@gmail.com>
2017-06-21 16:41:47 -0400

    starting work on using protocol buffer messages

--- beacon/command_processor.go
+++ beacon/command_processor.go
@@ -4,6 +4,8 @@ import "log"
 import "sync"
 import "bytes"
 import "github.com/hink/go-blink1"
+import "github.com/golang/protobuf/proto"
+import "github.com/dadleyy/beacon.api/beacon/interchange"
 
 type messageHeader struct {
 	DeviceId string `json:"device_id"`
@@ -19,7 +21,14 @@ func (processor *CommandProcessor) Start(wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	for buffer := range processor.CommandStream {
-		processor.Printf("received: %s", buffer)
-		processor.Device.SetState(blink1.State{})
+		message := interchange.DeviceMessage{}
+
+		if e := proto.Unmarshal(buffer.Bytes(), &message); e != nil {
+			processor.Printf("receved strange message: %s", e.Error())
+			continue
+		}
+
+		processor.Printf("received: %s", message.RequestPath)
+		processor.Device.SetState(blink1.State{Red: 255})
 	}
 }
