e164dd8d099458123cfec02f612837135a4d8b5b
Danny Hadley <dadleyy@gmail.com>
2017-06-22 23:00:48 -0400

    adding hearbeat delay to command line options

--- beacon/heartbeat_processor.go
+++ beacon/heartbeat_processor.go
@@ -7,21 +7,23 @@ import "time"
 import "github.com/gorilla/websocket"
 
 // NewHeartbeatProcessor creates a new processor for heartbeats
-func NewHeartbeatProcessor(connection *websocket.Conn) *HeartbeatProcessor {
+func NewHeartbeatProcessor(connection *websocket.Conn, delay time.Duration) *HeartbeatProcessor {
 	logger := log.New(os.Stdout, "heartbeat", log.Ldate|log.Ltime|log.Lshortfile)
-	return &HeartbeatProcessor{logger, connection}
+	return &HeartbeatProcessor{logger, delay, connection}
 }
 
 // HeartbeatProcessor is responsible for keeping the websocket connection alive
 type HeartbeatProcessor struct {
 	*log.Logger
+	delay      time.Duration
 	connection *websocket.Conn
 }
 
 // Start launches the hearbeat sequence
 func (processor *HeartbeatProcessor) Start(wg *sync.WaitGroup) {
 	defer wg.Done()
-	ticker := time.NewTicker(time.Minute)
+	ticker := time.NewTicker(processor.delay)
+	processor.Printf("heartbeat processor starting")
 
 	for _ = range ticker.C {
 		writer, e := processor.connection.NextWriter(websocket.TextMessage)
