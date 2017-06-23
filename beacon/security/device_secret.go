906b048cf094a2b4e5ff1fbaf3dfa0f83006c95f
Danny Hadley <dadleyy@gmail.com>
2017-06-23 19:34:03 -0400

    adding device secret security token to registration request

---
+++ beacon/security/device_secret.go
@@ -0,0 +1,47 @@
+package security
+
+import "fmt"
+import "io/ioutil"
+import "crypto/rsa"
+import "crypto/x509"
+import "encoding/pem"
+import "encoding/hex"
+
+// DeviceKey objects contain the rsa private key used to secure communications w/ the api
+type DeviceKey struct {
+	*rsa.PrivateKey
+}
+
+// SharedSecret returns the string version of the rsa public key
+func (key *DeviceKey) SharedSecret() (string, error) {
+	publicKeyData, e := x509.MarshalPKIXPublicKey(key.Public())
+
+	if e != nil {
+		return "", e
+	}
+
+	return hex.EncodeToString(publicKeyData), nil
+}
+
+// ReadDeviceKeyFromFile returns a new device key from a filename
+func ReadDeviceKeyFromFile(filename string) (*DeviceKey, error) {
+	privateKeyData, e := ioutil.ReadFile(filename)
+
+	if e != nil {
+		return nil, e
+	}
+
+	privateBlock, _ := pem.Decode(privateKeyData)
+
+	if privateBlock == nil {
+		return nil, fmt.Errorf("invalid-pem")
+	}
+
+	privateKey, e := x509.ParsePKCS1PrivateKey(privateBlock.Bytes)
+
+	if e != nil {
+		return nil, e
+	}
+
+	return &DeviceKey{privateKey}, nil
+}
--- main.go
+++ main.go
@@ -8,11 +8,13 @@ import "flag"
 import "sync"
 import "time"
 import "bytes"
+import "net/http"
 
 import "github.com/hink/go-blink1"
 import "github.com/gorilla/websocket"
 import "github.com/dadleyy/beacon.client/beacon"
 import "github.com/dadleyy/beacon.client/beacon/defs"
+import "github.com/dadleyy/beacon.client/beacon/security"
 
 func main() {
 	options := struct {
@@ -20,12 +22,14 @@ func main() {
 		debugging      bool
 		commandBuffer  int
 		heartbeatDelay int
+		privateKeyfile string
 	}{}
 
 	flag.StringVar(&options.apiHome, "api", "0.0.0.0:12345", "the hostname of the beacon.api server")
 	flag.BoolVar(&options.debugging, "debug", false, "if true, the client will not attempt to open the blink device")
 	flag.IntVar(&options.commandBuffer, "command-buffer", 2, "amount of allowed commands to buffer")
 	flag.IntVar(&options.heartbeatDelay, "heartbeat-delay", 10, "amount of seconds between heartbeat pings")
+	flag.StringVar(&options.privateKeyfile, "privte-key", ".keys/private.pem", "the filename of the private key")
 	flag.Parse()
 
 	if len(options.apiHome) < 1 {
@@ -35,6 +39,20 @@ func main() {
 
 	logger := log.New(os.Stdout, "beacon client", log.Ldate|log.Ltime|log.Lshortfile)
 	var device beacon.Commandable
+
+	key, e := security.ReadDeviceKeyFromFile(options.privateKeyfile)
+
+	if e != nil {
+		logger.Printf("invalid file name: %s", e.Error())
+		return
+	}
+
+	sharedSecret, e := key.SharedSecret()
+
+	if e != nil {
+		logger.Printf("invalid file name: %s", e.Error())
+		return
+	}
 
 	if options.debugging {
 		device = &beacon.StateLogger{log.New(os.Stdout, "beacon log device", log.Ldate|log.Ltime|log.Lshortfile)}
@@ -50,13 +68,16 @@ func main() {
 
 	defer device.Close()
 	defer device.SetState(blink1.State{})
-	dialer, endpoint := websocket.Dialer{}, fmt.Sprintf("ws://%s/%s", options.apiHome, defs.APIRegistrationEndpoint)
+	dialer, apiUrl := websocket.Dialer{}, fmt.Sprintf("ws://%s/%s", options.apiHome, defs.APIRegistrationEndpoint)
+	header := http.Header{}
 
-	logger.Printf("opening connection to api: %s", endpoint)
-	ws, _, e := dialer.Dial(endpoint, nil)
+	header.Set(defs.APIAuthorizationHeader, sharedSecret)
+
+	logger.Printf("opening connection to api: %s", apiUrl)
+	ws, _, e := dialer.Dial(apiUrl, header)
 
 	if e != nil {
-		logger.Fatalf("unable to connect to %s: %s", endpoint, e.Error())
+		logger.Fatalf("unable to connect to %s: %s", apiUrl, e.Error())
 		return
 	}
 
